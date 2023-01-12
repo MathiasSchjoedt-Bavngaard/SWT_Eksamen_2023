ba510a1bffa48672e57af56df9c8b52e1f25c323
MathiasSchjoedt-Bavngaard <81029612+MathiasSchjoedt-Bavngaard@users.noreply.github.com>
2023-01-12 20:41:09 +0100

    Added Test To UserInterfaceTest

--- Test.Unit/UserInterfaceTest.cs
+++ Test.Unit/UserInterfaceTest.cs
@@ -27,6 +27,40 @@ namespace Test.Unit
         {
             _godkendLaan.Ansoeg(beloeb, varighed, indtaegt, udgifter).Returns(true);
             Assert.That(_uut.Ansoeg(beloeb, varighed, indtaegt, udgifter), Is.EqualTo(true));
+            
+        }
+
+        [TestCase(900, 1234)]
+        [TestCase(20000, 12345)]
+        [TestCase(3000, 123456)]
+        [TestCase(100000, 1234567)]
+        public void FriGivLaan_overNul(double beloeb, int kontonummer)
+        {
+            _godkendLaan.FrigivLaan(beloeb, kontonummer).Returns(true);
+            Assert.That(_uut.FrigivLaan(beloeb, kontonummer), Is.EqualTo(true));
+        }
+        
+        [Test]
+        public void _godkendLaan_Calls_ansoeg()
+        {
+            _uut.Ansoeg(10000, 12, 100000, 10000);
+            _godkendLaan.Received(1).Ansoeg(10000, 12, 100000, 10000);
+        }
+
+        [Test]
+        public void _godkendLaan_Calls_frigivLaan()
+        {
+            _uut.FrigivLaan(10000, 1234);
+            _godkendLaan.Received(1).FrigivLaan(10000, 1234);
+        }
+
+        [Test]
+        public void Properties_Set_Get()
+        {
+            _uut._godkendLaan = _godkendLaan;
+            _uut._display = _display;
+            Assert.That(_uut._godkendLaan, Is.EqualTo(_godkendLaan));
+            Assert.That(_uut._display, Is.EqualTo(_display));
         }
 
 
