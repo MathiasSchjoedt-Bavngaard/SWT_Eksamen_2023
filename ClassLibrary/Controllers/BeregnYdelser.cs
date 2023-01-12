4c6c0ea8f6797370404b110ad3d72f79913d047c
MathiasSchjoedt-Bavngaard <81029612+MathiasSchjoedt-Bavngaard@users.noreply.github.com>
2023-01-12 13:05:16 +0100

    Added Construktor and Rente Handler

--- ClassLibrary/Controllers/BeregnYdelser.cs
+++ ClassLibrary/Controllers/BeregnYdelser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary.Boundry;
 using ClassLibrary.Interfaces;
 
 namespace ClassLibrary.Controllers
@@ -11,29 +12,70 @@ namespace ClassLibrary.Controllers
     {
         public double AktuelRente { get; private set; }
 
-        // Her mangler constructor
+        private IRenteserverInterface _renteserver;
+        private IDisplay _display;
 
-        // Her mangler en event handler
+        public BeregnYdelser()
+        {
+            _renteserver = new RenteserverInterface();
+            _renteserver.NyRente += new EventHandler(RenteSkiftet);
+            _display = new Display();
+            AktuelRente = 0.05;
+        }
+        public BeregnYdelser(IRenteserverInterface renteserver)
+        {
+            _renteserver = renteserver;
+            _renteserver.NyRente += new EventHandler(RenteSkiftet);
+            _display = new Display();
+            AktuelRente = 0.05;
+        }
+        public BeregnYdelser(IRenteserverInterface renteserver, IDisplay display)
+        {
+            _renteserver = renteserver;
+            _renteserver.NyRente += new EventHandler(RenteSkiftet);
+            _display = display;
+            AktuelRente = 0.05;
+        }
+        public BeregnYdelser(IDisplay display)
+        {
+            _renteserver = new RenteserverInterface();
+            _renteserver.NyRente += new EventHandler(RenteSkiftet);
+            _display = display;
+            AktuelRente = 0.05;
+        }
 
+        private void RenteSkiftet(object? sender, EventArgs e)
+        {
+            AktuelRente = _renteserver.Rente;
+            OpdaterRenteDisplayed();
+        }
+
+        private void OpdaterRenteDisplayed()
+        {
+            _display.OpdaterLaaneRente(AktuelRente);
+        }
+        
+        
         // Denne metode er implementeret, men skal testes
         // Den bruger annuitetsformlen
+
         public double BeregnYdelse(double beloeb, int varighed)
+    {
+        double ydelse = double.MaxValue;
+
+        if (varighed >= 1 && varighed <= 120)
         {
-            double ydelse = double.MaxValue;
-
-            if (varighed >= 1 && varighed <= 120)
+            if (AktuelRente == 0)
             {
-                if (AktuelRente == 0)
-                {
-                    ydelse = beloeb / varighed;
-                }
-                else
-                {
-                    ydelse = beloeb * AktuelRente / (1 - Math.Pow(1 + AktuelRente, -varighed));
-                }
+                ydelse = beloeb / varighed;
             }
-
-            return ydelse;
+            else
+            {
+                ydelse = beloeb * AktuelRente / (1 - Math.Pow(1 + AktuelRente, -varighed));
+            }
         }
+
+        return ydelse;
+    }
     }
 }
