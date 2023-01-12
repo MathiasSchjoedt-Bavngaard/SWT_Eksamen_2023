116410c0820939aeb0e3a36c34c1d731fb29ab94
MathiasSchjoedt-Bavngaard <81029612+MathiasSchjoedt-Bavngaard@users.noreply.github.com>
2023-01-12 21:27:50 +0100

    Changed IntergrationTest to just Say "not implementet"

--- Test.Integration/IntegrationTest1.cs
+++ Test.Integration/IntegrationTest1.cs
@@ -4,24 +4,6 @@ namespace Test.Intergration
 {
     public class IntergrationTests
     {
-
-        IGodkendLaan _controllerClass2;
-       
-
-
-        [SetUp]
-        public void Setup()
-        {
-            
-            _controllerClass2 = new GodkendLaan();
-        }
-
-        [Test]
-        public void ClassInController()
-        {
-            
-            Assert.That(4, Is.EqualTo(4));
-        }
-
+        //notImplementet
     }
 }
